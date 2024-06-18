422fc1b7219c76bc1dd17427a798f118f1915284
shunta <shunta.gdo@gmail.com>
2024-06-18 07:09:21 +0000

    add column and validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -41,8 +41,14 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         //
+        $request->validate([
+            'content'=>'required',
+            'status'=>'required|max:10',
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         
         return redirect('/');
@@ -79,10 +85,16 @@ class TasksController extends Controller
      */
     public function update(Request $request, string $id)
     {
+        $request->validate([
+            'content'=>'required',
+            'status'=>'required|max:10',
+        ]);
+        
         //
         $task = Task::findOrFail($id);
         
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         
         return redirect(route('tasks.index'));;
