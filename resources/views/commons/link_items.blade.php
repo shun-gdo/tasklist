994426d5f6aaccd68dc461e6e73df8bb6e6bf68d
shunta <shunta.gdo@gmail.com>
2024-07-05 07:14:13 +0000

    fix nav style

--- resources/views/commons/link_items.blade.php
+++ resources/views/commons/link_items.blade.php
@@ -1,14 +1,14 @@
 @if (Auth::check())
 
-    <li><a class="link link-hover" href="#">Tasklist</a></li>
+    <li><a class="link link-hover" href="/">Tasklist</a></li>
 
     
-    <li><a class="link link-hover" href="#">
+    <li class="ml-3"><a class="link link-hover" href="{{ route('profile.edit') }}">
         Logging in as {{ Auth::user()->name }}
         <span>({{ Auth::user()->email }})</span>
     </a></li>
 
-    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
+    <li class="ml-3"><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
 @else
     {{-- ユーザー登録ページへのリンク --}}
     <li><a class="link link-hover" href="{{ route('register') }}">Signup</a></li>
